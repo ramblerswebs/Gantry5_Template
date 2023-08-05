4f6a476463eb65771b225bf023b81426b512a7d8
keithgrimes <keith.grimes@kcgrimes.co.uk>
2023-08-05 22:12:04 +0100

    Update Installer

--- install.php
+++ install.php
@@ -10,6 +10,11 @@
  */
 defined('_JEXEC') or die;
 
+use Gantry\Framework\Gantry;
+use Gantry\Framework\ThemeInstaller;
+use Gantry5\Loader;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Installer\InstallerAdapter;
 use Joomla\Filesystem\File;
 use Joomla\Filesystem\Folder;
@@ -19,45 +24,74 @@ use Joomla\Filesystem\Folder;
  */
 class tpl_hydrogen_ramblersInstallerScript
 {
-	function install($parent)
-	{
-			JFactory::getApplication()->enqueueMessage('Ramblers customisation of Hydrogen Template is being installed.');
-	}
-    /**
-     * @param InstallerAdapter $parent
-     * @return bool
-     */
-    public function uninstall($parent)
+    /** @var string */
+    public $requiredGantryVersion = '5.5';
+
+    public function preflight($type, $parent)
     {
-        // Remove all Nucleus files manually as file installer only uninstalls files.
-        $manifest = $parent->getManifest();
-
-        // Loop through all elements and get list of files and folders
-        foreach ($manifest->fileset->files as $eFiles) {
-            $target = (string) $eFiles->attributes()->target;
-            $targetFolder = empty($target) ? JPATH_ROOT : JPATH_ROOT . '/' . $target;
-
-            // Check if all children exists
-            if (count($eFiles->children()) > 0) {
-                // Loop through all filenames elements
-                foreach ($eFiles->children() as $eFileName) {
-                    if ($eFileName->getName() === 'folder')
-                    {
-                        $folder = $targetFolder . '/' . $eFileName;
-
-                        $files = Folder::files($folder, '.', false, true);
-                        foreach ($files as $name) {
-                            File::delete($name);
-                        }
-                        $subFolders = Folder::folders($folder, '.', false, true);
-                        foreach ($subFolders as $name) {
-                            Folder::delete($name);
-                        }
-                    }
-                }
-            }
+        if ($type === 'uninstall') {
+            return true;
         }
-		JFactory::getApplication()->enqueueMessage('Ramblers customisation of the Hydrogen Template is being removed.');
+
+        $manifest = $parent->getManifest();
+        $name = Text::_($manifest->name);
+
+        // Prevent installation if Gantry 5 isn't enabled or is too old for this template.
+        try {
+            if (!class_exists('Gantry5\Loader')) {
+                throw new RuntimeException(sprintf('Please install Gantry 5 Framework before installing %s template!', $name));
+            }
+
+            Loader::setup();
+
+            $gantry = Gantry::instance();
+
+            if (!method_exists($gantry, 'isCompatible') || !$gantry->isCompatible($this->requiredGantryVersion)) {
+                throw new \RuntimeException(sprintf('Please upgrade Gantry 5 Framework to v%s (or later) before installing %s template!', strtoupper($this->requiredGantryVersion), $name));
+            }
+
+        } catch (Exception $e) {
+            $app = Factory::getApplication();
+            $app->enqueueMessage(Text::sprintf($e->getMessage()), 'error');
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string $type
+     * @param object $parent
+     * @throws Exception
+     */
+    public function postflight($type, $parent)
+    {
+        if ($type === 'uninstall') {
+            return true;
+        }
+
+        //$installer = new ThemeInstaller($parent);
+        //$installer->initialize();
+
+        // Install sample data on first install.
+        if (in_array($type, array('update'))) {
+            try {
+                //$installer->installDefaults();
+
+                //echo $installer->render('install.html.twig');
+                echo "Hello";
+
+            } catch (Exception $e) {
+                $app = Factory::getApplication();
+                $app->enqueueMessage(Text::sprintf($e->getMessage()), 'error');
+            }
+        } else {
+            //echo $installer->render('update.html.twig');
+            echo "Hello 2";
+        }
+
+        //$installer->finalize();
 
         return true;
     }
